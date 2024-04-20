215dcb60e2c50e377f287abba176b3802fc72a07
zeenee <cabrerarizzi@gmail.com>
2024-04-21 01:38:06 +0800

    code the base html architecture

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import './App.css';
 function App() {
   return (
     <>
-    
+     <header>
+      <h1 className='header'>Generate an Excuse!</h1>
+     </header>
+
+     <section>
+      <div className='excuse-type'>
+        <h3>Family</h3>
+        <button>Generate</button>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum malesuada justo, eget gravida orci.</p>
+      </div>
+     </section>
     </>
   );
 }
