b70fdee602038813091d0c199c3a08a32bc562d8
zeenee <cabrerarizzi@gmail.com>
2024-04-21 02:39:21 +0800

    add remaining excuse types and add style to all elements

--- src/App.js
+++ src/App.js
@@ -8,10 +8,60 @@ function App() {
      </header>
 
      <section>
-      <div className='excuse-type'>
-        <h3>Family</h3>
-        <button>Generate</button>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum malesuada justo, eget gravida orci.</p>
+      <div className='inner-container'>
+        <div className='excuse-type'>
+          <h3>Family</h3>
+          <button>Generate</button>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum malesuada justo, eget gravida orci.</p>
+        </div>
+
+        <div className='excuse-type'>
+          <h3>Office</h3>
+          <button>Generate</button>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum malesuada justo, eget gravida orci.</p>
+        </div>
+
+        <div className='excuse-type'>
+          <h3>Children</h3>
+          <button>Generate</button>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum malesuada justo, eget gravida orci.</p>
+        </div>
+
+        <div className='excuse-type'>
+          <h3>College</h3>
+          <button>Generate</button>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum malesuada justo, eget gravida orci.</p>
+        </div>
+
+        <div className='excuse-type'>
+          <h3>Party</h3>
+          <button>Generate</button>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum malesuada justo, eget gravida orci.</p>
+        </div>
+
+        <div className='excuse-type'>
+          <h3>Funny</h3>
+          <button>Generate</button>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum malesuada justo, eget gravida orci.</p>
+        </div>
+        
+        <div className='excuse-type'>
+          <h3>Unbelievable</h3>
+          <button>Generate</button>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum malesuada justo, eget gravida orci.</p>
+        </div>
+
+        <div className='excuse-type'>
+          <h3>Developers</h3>
+          <button>Generate</button>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum malesuada justo, eget gravida orci.</p>
+        </div>
+
+        <div className='excuse-type'>
+          <h3>Gaming</h3>
+          <button>Generate</button>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum malesuada justo, eget gravida orci.</p>
+        </div>
       </div>
      </section>
     </>
