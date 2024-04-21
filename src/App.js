1ccac1c72aa5cf29191f37131e412f89a8c09978
zeenee <cabrerarizzi@gmail.com>
2024-04-21 18:04:30 +0800

    move entire excuse-type div into a component

--- src/App.js
+++ src/App.js
@@ -15,126 +15,115 @@ function App() {
   const [unbExcuse, setUnbelievableExcuse] = useState('');
   const [devExcuse, setDevelopersExcuse] = useState('');
   const [gamExcuse, setGamingExcuse] = useState('');
+  const [generatedExcuse, setgeneratedExcuse] = useState(null);
 
   const familyExcuse = () => {
     Axios.get('https://excuser-three.vercel.app/v1/excuse/family/').then((res) => {
       setFamilyExcuse(res.data[0].excuse);
+
+      if (famExcuse != '') {
+        setgeneratedExcuse('"'+famExcuse+'"');
+      } else {
+        setgeneratedExcuse('Hold on my brain for Family Excuses just woke up! Please click me once more 🙏');
+      }
     })
   }
   
   const officeExcuse = () => {
     Axios.get('https://excuser-three.vercel.app/v1/excuse/office/').then((res) => {
       setOfficeExcuse(res.data[0].excuse);
+      if (offExcuse != '') {
+        setgeneratedExcuse('"'+offExcuse+'"');
+      } else {
+        setgeneratedExcuse('Hold on my brain for Office Excuses just woke up! Please click me once more 🙏');
+      }
     })
   }
   
   const childrenExcuse = () => {
     Axios.get('https://excuser-three.vercel.app/v1/excuse/children/').then((res) => {
       setChildrenExcuse(res.data[0].excuse);
+      setgeneratedExcuse('"'+chiExcuse+'"');
     })
   }
   
   const collegeExcuse = () => {
     Axios.get('https://excuser-three.vercel.app/v1/excuse/college/').then((res) => {
       setCollegeExcuse(res.data[0].excuse);
+      setgeneratedExcuse('"'+colExcuse+'"');
     })
   }
   
   const partyExcuse = () => {
     Axios.get('https://excuser-three.vercel.app/v1/excuse/party/').then((res) => {
       setPartyExcuse(res.data[0].excuse);
+      setgeneratedExcuse('"'+parExcuse+'"');
     })
   }
   
   const funnyExcuse = () => {
     Axios.get('https://excuser-three.vercel.app/v1/excuse/funny/').then((res) => {
       setFunnyExcuse(res.data[0].excuse);
+      setgeneratedExcuse('"'+funExcuse+'"');
     })
   }
   
   const unbelievableExcuse = () => {
     Axios.get('https://excuser-three.vercel.app/v1/excuse/unbelievable/').then((res) => {
       setUnbelievableExcuse(res.data[0].excuse);
+      setgeneratedExcuse('"'+unbExcuse+'"');
     })
   }
   
   const developersExcuse = () => {
     Axios.get('https://excuser-three.vercel.app/v1/excuse/developers/').then((res) => {
       setDevelopersExcuse(res.data[0].excuse);
+      setgeneratedExcuse('"'+devExcuse+'"');
     })
   }
   
   const gamingExcuse = () => {
     Axios.get('https://excuser-three.vercel.app/v1/excuse/gaming/').then((res) => {
       setGamingExcuse(res.data[0].excuse);
+      setgeneratedExcuse('"'+gamExcuse+'"');
     })
   }
 
   return (
-    <>
-     <header>
-      <h1 className='header'>Generate a Silly Excuse!</h1>
-     </header>
+    <div className='main-container'>
+      <div className='left-section'>
+        <header>
+          <h1 className='header'>Generate a Silly Excuse!</h1>
+        </header>
+        <article>{generatedExcuse}</article>
+      </div>
+
 
      <section>
       <div className='inner-container'>
-        <div className='excuse-type'>
-          <h3>Family</h3>
-          <button onClick={familyExcuse}>Generate</button>
-          <p>{famExcuse}</p>
-        </div>
-
-        <div className='excuse-type'>
-          <h3>Office</h3>
-          <button onClick={officeExcuse}>Generate</button>
-          <p>{offExcuse}</p>
-        </div>
-
-        <div className='excuse-type'>
-          <h3>Children</h3>
-          <button onClick={childrenExcuse}>Generate</button>
-          <p>{chiExcuse}</p>
-        </div>
-
-        <div className='excuse-type'>
-          <h3>College</h3>
-          <button onClick={collegeExcuse}>Generate</button>
-          <p>{colExcuse}</p>
-        </div>
-
-        <div className='excuse-type'>
-          <h3>Party</h3>
-          <button onClick={partyExcuse}>Generate</button>
-          <p>{parExcuse}</p>
-        </div>
-
-        <div className='excuse-type'>
-          <h3>Funny</h3>
-          <button onClick={funnyExcuse}>Generate</button>
-          <p>{funExcuse}</p>
-        </div>
-        
-        <div className='excuse-type'>
-          <h3>Unbelievable</h3>
-          <button onClick={unbelievableExcuse}>Generate</button>
-          <p>{unbExcuse}</p>
-        </div>
-
-        <div className='excuse-type'>
-          <h3>Developers</h3>
-          <button onClick={developersExcuse}>Generate</button>
-          <p>{devExcuse}</p>
-        </div>
-
-        <div className='excuse-type'>
-          <h3>Gaming</h3>
-          <button onClick={gamingExcuse}>Generate</button>
-          <p>{gamExcuse}</p>
-        </div>
+        <Excuse type='Family' toClick={familyExcuse} />
+        <Excuse type='Office' toClick={officeExcuse}/>
+        <Excuse type='Children' toClick={childrenExcuse}/>
+        <Excuse type='College' toClick={collegeExcuse}/>
+        <Excuse type='Party' toClick={partyExcuse}/>
+        <Excuse type='Funny' toClick={funnyExcuse}/>
+        <Excuse type='Unbelievable' toClick={unbelievableExcuse}/>
+        <Excuse type='Developers' toClick={developersExcuse}/>
+        <Excuse type='Gaming' toClick={gamingExcuse}/>
       </div>
      </section>
-    </>
+    </div>
   );
 }
 
+function Excuse(props) {
+  return (
+    <div className='excuse-type'>
+      <h3>{props.type}</h3>
+      <div className='excuse-type__button-container'>
+        <button onClick={props.toClick}>Generate</button>
+      </div>
+    </div>
+  )
+}
 export default App;
